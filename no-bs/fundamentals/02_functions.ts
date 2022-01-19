7da27faddbb7f5e6ad31d86c023d7b1d8b927c20
Jay Monari <93833532+JayMonari@users.noreply.github.com>
2022-01-19 08:23:43 -0500

    All the different ways to write function signatures and show compile time != runtime

--- no-bs/fundamentals/02_functions.ts
+++ no-bs/fundamentals/02_functions.ts
@@ -2,4 +2,24 @@ function addNumbers(a: number, b: number): number {
   return a + b;
 }
 
+export const addStrings = (s1: string, s2: string): string => `${s1} ${s2}`;
+
+export const format = (title: string, param: string | number): string =>
+  `${title} ${param}`;
+
+export const printFormat = (title: string, param: string | number): void =>
+  console.log(`${title} ${param}`);
+
+export const fetchData = (url: string): Promise<string> =>
+  Promise.resolve(`Data from ${url}`);
+
+export function introduce(greeting: string, ...names: string[]): string {
+  return `${greeting} ${names.join(" ")}`;
+}
+
+// XXX: TypeScript only works at compile time NOT at runtime.
+export function getName(user: { first: string; last: string }): string {
+  return `${user?.first ?? "First"} ${user?.last ?? "Last"}`;
+}
+
 export default addNumbers;
--- no-bs/fundamentals/02_functions_test.ts
+++ no-bs/fundamentals/02_functions_test.ts
@@ -1,4 +1,6 @@
 // @ts-ignore
-import addNumbers from "./02_functions.ts";
+import addNumbers, {addStrings} from "./02_functions.ts";
 
 console.log(addNumbers(1, 2));
+console.log(addStrings("a", "b"));
+console.log(addStrings("a"));
